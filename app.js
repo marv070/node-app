a67aaf87657f57a2fe6a33986fb7741c3ba3b339
Marvin Laucher <marv070@gmail.com>
2017-08-17 08:32:24 -0400

    updated bg color of index

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+// app.get('/', function(req,resp){
+//   resp.sendFile('home.html', {root: path.join(__dirname, './views')});
+
+// });
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
